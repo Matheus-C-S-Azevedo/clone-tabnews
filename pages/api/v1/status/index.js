7f04dc7d223f5ed81dc1a5889620a0a65604916a
Matheus-C-S-Azevedo <matheus.costa.s.azevedo@gmail.com>
2026-02-25 16:18:36 -0400

    Finish `api\v1\status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(resquest, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Resposta: "Tudo ok" });
+  const updatedAt = new Date().toISOString();
+  const pgVersion = await database.query("SHOW server_version;");
+  const pgMaxConnections = await database.query("SHOW max_connections;");
+  const pgCurrentConnected = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion.rows[0].server_version,
+        max_connections: Number(pgMaxConnections.rows[0].max_connections),
+        opened_connections: Number(pgCurrentConnected.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
